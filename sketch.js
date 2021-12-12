02b256ebb4b8178e33d0b554c4f9d1a676d1dca8
AhnHongju <93176798+AhnHongju@users.noreply.github.com>
2021-12-13 00:28:23 +0900

    image load

--- sketch.js
+++ sketch.js
@@ -1,16 +1,24 @@
 let arrayLetter=[];
 let size=400;
+let img;
+
+function preload()
+{
+    img = loadImage("nature.webp");
+}
 function setup() {
   createCanvas(800, 800);
+  background(0);
+
   for(let i=0;i<size;i++)
   {
     arrayLetter.push(new Particle());
   }
-   
+
 }
 
 function draw() {
-  background(0);
+  image(img,0,0,width,height);
   for(let i=0;i<size;i++)
     {
       arrayLetter[i].update();
