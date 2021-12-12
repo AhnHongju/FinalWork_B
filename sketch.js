f4a357f3be317e8fd50842fb39e377b4bd3ca385
AhnHongju <93176798+AhnHongju@users.noreply.github.com>
2021-12-13 02:22:17 +0900

    이상해져서 다시....
    그림의 색을 추출하여 글자 색을 칠하기

--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
 let arrayLetter=[];
-let size=300;
+let size=100;
 let img;
+let c;
 
 function preload()
 {
-    img = loadImage("nature.webp");
+    img = loadImage("nature2.jpg");
 }
-function setup() {
+function setup()
+{
   createCanvas(800, 800);
+  image(img,0,0,width,height);
   background(0);
+
 
   for(let i=0;i<size;i++)
   {
@@ -17,10 +21,13 @@ function setup() {
 
 }
 
-function draw() {
-  image(img,0,0,width,height);
+function draw()
+{
+
   for(let i=0;i<size;i++)
     {
+      c=img.get(arrayLetter[i].loc.x,arrayLetter[i].loc.y);
+      fill(c);
       arrayLetter[i].update();
       arrayLetter[i].display();
     }
