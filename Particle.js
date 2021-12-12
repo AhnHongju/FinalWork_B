85ca48ac7994e1ffbb4aa96264503d6f0887f47d
AhnHongju <93176798+AhnHongju@users.noreply.github.com>
2021-12-13 02:10:18 +0900

    loadimg

--- Particle.js
+++ Particle.js
@@ -1,25 +1,32 @@
 class Particle
   {
-    constructor(size)
+    constructor()
     {
       this.loc=createVector(width/2,height/2);
       this.vel=createVector(0,0);
-      this.w=size;
-  
       stroke(255,255,255,50);
-      
+
     }
-    
+
     update()
     {
+      textAlign(CENTER);
       let acc=createVector(random(-1,1),random(-1,1));
       this.loc.add(this.vel);
       this.vel.add(acc);
+      if(this.loc.x>width||this.loc.x<0)
+      {
+        this.vel.x*=-1;
+      }
+      if(this.loc.y>height||this.loc.y<0)
+      {
+        this.vel.y*=-1;
+      }
     }
     display()
     {
+      noStroke();
       text("NATURE",this.loc.x,this.loc.y);
-      fill(255);
-      textSize(10);
+      textSize(5);
     }
   }
